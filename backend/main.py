3fa5e47f1e280c52eb0901b75e3b2d2493cad7ce
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-09-28 13:41:48 +0200

    add fastapi

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/jesus")
+async def jesus() -> dict[str, str]:
+    return {"message": "Jesus is the way, the truth, and the life."}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
