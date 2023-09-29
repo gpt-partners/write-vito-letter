64a17e28b018b5b44657619fb1981434a8212242
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-09-29 20:28:50 +0200

    add pydantic

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,23 @@
 # https://www.pinecone.io/learn/fast-retrieval-augmented-generation/
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/jesus")
-async def jesus() -> dict[str, str]:
+class VitoResponse(BaseModel):
+    message: str
+
+
+class VitoRequest(BaseModel):
+    companyUrl: str
+    linkedinUrl: str
+
+
+@app.post("/vito-message")
+async def vito_message(vitoRequest: VitoRequest) -> VitoResponse:
     return {"message": "Jesus is the way, the truth, and the life."}
+
 
 
 if __name__ == "__main__":
